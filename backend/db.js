8916ac7e8f88c61edc6593c96f064511e3f787c8
Arya Satam <150563115+aryanami@users.noreply.github.com>
2025-02-13 01:12:08 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'clinical_trials',
-  password: 'Apple@01',
+  database: name,
+  password: password,
   port: 5432,
 });
 
